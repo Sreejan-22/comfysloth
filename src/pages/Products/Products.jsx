cfd5646f235cd96d096e4d5452f19ea56ebb2abd
Sreejan <sreejan147@gmail.com>
2021-09-08 22:31:43 +0530

    feat: add sort section

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import Slider from "@material-ui/core/Slider";
+import ViewModuleIcon from "@material-ui/icons/ViewModule";
+import ViewListIcon from "@material-ui/icons/ViewList";
 import Navbar from "../../components/Navbar/Navbar";
 import BreadCrumbs from "../../components/Breadcrumbs/Breadcrumbs";
-import Button from "@material-ui/core/Button";
 import "./Products.scss";
 
 const breadcrumbArr = [
@@ -27,12 +28,12 @@ const categories = [
 ];
 
 const companies = ["All", "Marcos", "Liddy", "Ikea", "Caressa"];
+const sortBy = ["Price (Lowest)", "Price (Highest)", "Name(A-Z)", "Name(Z-A)"];
 
 const Products = () => {
   const [value, setValue] = useState(60000);
   const [free, setFree] = useState(false);
-
-  console.log(free);
+  const [gridView, setGridView] = useState(true);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -57,7 +58,9 @@ const Products = () => {
             <h3>Company</h3>
             <select name="company" className="company">
               {companies.map((item) => (
-                <option value={item}>{item}</option>
+                <option value={item} key={item}>
+                  {item}
+                </option>
               ))}
             </select>
             <h3>Price</h3>
@@ -81,7 +84,36 @@ const Products = () => {
           </form>
           <button className="clear-filters">Clear Filters</button>
         </div>
-        <div className="pdt-list"></div>
+        <div className="pdt-list">
+          <div className="view-filters">
+            <ViewModuleIcon
+              className={`view-icon ${gridView ? "active" : ""}`}
+              onClick={() => {
+                if (!gridView) {
+                  setGridView(true);
+                }
+              }}
+            />
+            <ViewListIcon
+              className={`view-icon ${!gridView ? "active" : ""}`}
+              onClick={() => {
+                if (gridView) {
+                  setGridView(false);
+                }
+              }}
+            />
+            <span>23 products found</span>
+            <hr />
+            <span>Sort By</span>
+            <select name="sortBy">
+              {sortBy.map((item) => (
+                <option value={item} key={item}>
+                  {item}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
       </div>
     </div>
   );
