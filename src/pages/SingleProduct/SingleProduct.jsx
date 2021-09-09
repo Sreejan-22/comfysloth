ab6c6ae8708d8bb8027ee40848f367d21cdb0fbb
Sreejan <sreejan147@gmail.com>
2021-09-09 19:57:34 +0530

    updated single products page

--- src/pages/SingleProduct/SingleProduct.jsx
+++ src/pages/SingleProduct/SingleProduct.jsx
@@ -1,5 +1,23 @@
 import { useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
+import BreadCrumbs from "../../components/Breadcrumbs/Breadcrumbs";
+import Footer from "../../components/Footer/Footer";
+
+const breadcrumbArr = [
+  {
+    text: "Home",
+    path: "/",
+  },
+  {
+    text: "Products",
+    path: "/products",
+  },
+  {
+    text: "Albany Sectional",
+    path: null,
+  },
+];
 
 const SingleProduct = () => {
   let { id } = useParams();
@@ -7,11 +25,17 @@ const SingleProduct = () => {
 
   useEffect(() => {
     console.log(id);
+    fetch(`https://course-api.com/react-store-single-product?id=${id}`)
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
   }, []);
 
   return (
     <div>
-      <h1>Single Product</h1>
+      <Navbar />
+      <BreadCrumbs arr={breadcrumbArr} />
+      <Footer />
     </div>
   );
 };
