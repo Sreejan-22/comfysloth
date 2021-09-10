8be70e12732455073539c09321c8e5ece653d059
Sreejan <sreejan147@gmail.com>
2021-09-10 13:15:32 +0530

    refactor

--- src/pages/SingleProduct/SingleProduct.jsx
+++ src/pages/SingleProduct/SingleProduct.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import { makeStyles } from "@material-ui/styles";
+import Rating from "@material-ui/lab/Rating";
 import Navbar from "../../components/Navbar/Navbar";
 import BreadCrumbs from "../../components/Breadcrumbs/Breadcrumbs";
 import Footer from "../../components/Footer/Footer";
@@ -31,6 +32,7 @@ const SingleProduct = () => {
   const history = useHistory();
   const [productData, setproductData] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [count, setCount] = useState(1);
 
   useEffect(() => {
     console.log(id);
@@ -65,7 +67,7 @@ const SingleProduct = () => {
               onClickFn={() => history.push("/products")}
               className={classes.backToProducts}
             />
-            <div className="pdt-details">
+            <div className="main-content">
               <div className="pdt-images">
                 <img
                   src={productData.images[0].url}
@@ -78,14 +80,42 @@ const SingleProduct = () => {
                   ))}
                 </div>
               </div>
-              <div className="pdt-decscription">
+              <div className="pdt-details">
                 <h1>{productData.name}</h1>
                 <br />
+                <h3 className="pdt-price">&#8377; {productData.price}</h3>
                 <br />
-                <h3>&#8377;{productData.price}</h3>
+                <div className="pdt-review">
+                  <Rating name="read-only" value={productData.stars} readOnly />
+                  <div>
+                    &nbsp;&nbsp; ({productData.reviews} customer reviews)
+                  </div>
+                </div>
                 <br />
+                <div className="pdt-desc">{productData.description}</div>
                 <br />
-                <div>{productData.description}</div>
+                <div className="pdt-info">
+                  <span>Availability:&nbsp;&nbsp;&nbsp;</span>
+                  <>{productData.stock ? "In stock" : "Out of stock"}</>
+                </div>
+                <div className="pdt-info">
+                  <span>SKU:&nbsp;&nbsp;&nbsp;</span>
+                  <>{productData.id}</>
+                </div>
+                <div className="pdt-info">
+                  <span>Brand:&nbsp;&nbsp;&nbsp;</span>
+                  <>{productData.company}</>
+                </div>
+                <div className="pdt-count">
+                  <span style={{ position: "relative", top: "-10px" }}>_</span>
+                  <div>{count}</div>
+                  <span>+</span>
+                </div>
+                <StyledButton
+                  text="Add to Card"
+                  // onClickFn={() => history.push("/products")}
+                  className={`${classes.backToProducts}`}
+                />
               </div>
             </div>
           </div>
