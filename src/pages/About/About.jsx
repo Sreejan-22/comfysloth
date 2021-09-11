45ed0e72629ff00189f697cb442e4166509738f9
Sreejan <sreejan147@gmail.com>
2021-09-11 18:29:30 +0530

    feat: updated About page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,41 @@
+import Navbar from "../../components/Navbar/Navbar";
+import BreadCrumbs from "../../components/Breadcrumbs/Breadcrumbs";
+import Footer from "../../components/Footer/Footer";
+import store from "../../assets/store.jpeg";
+import "./About.scss";
+
+const breadcrumbArr = [
+  {
+    text: "Home",
+    path: "/",
+  },
+  {
+    text: "About",
+    path: null,
+  },
+];
+
 const About = () => {
   return (
     <div>
-      <h1>About</h1>
+      <Navbar />
+      <BreadCrumbs arr={breadcrumbArr} />
+      <div className="about-content">
+        <div>
+          <img src={store} alt="" />
+        </div>
+        <div>
+          <h1>Our Story</h1>
+          <div className="under-line"></div>
+          <br />
+          <br />
+          <h4>
+            A long tradition of making furniture now becomes an online venture.
+            Our mission is to create world-class and best quality products.
+          </h4>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
