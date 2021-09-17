b01da4fa33e6c753a4db5c037b721a333fa64b9c
Sreejan <sreejan147@gmail.com>
2021-09-17 17:15:14 +0530

    docs: install stripe,react-stripe; update Checkout

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,3 +1,7 @@
+import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { cartSelector } from "../../slices/cart.slice";
+import StyledButton from "../../components/StyledButton/StyledButton";
 import Navbar from "../../components/Navbar/Navbar";
 import BreadCrumbs from "../../components/Breadcrumbs/Breadcrumbs";
 import Footer from "../../components/Footer/Footer";
@@ -15,10 +19,35 @@ const breadcrumbArr = [
 ];
 
 const Checkout = () => {
+  const history = useHistory();
+  const { cartItems } = useSelector(cartSelector);
+
   return (
     <div>
       <Navbar />
       <BreadCrumbs arr={breadcrumbArr} />
+      {!cartItems.length ? (
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <br />
+          <br />
+          <br />
+          <h1>Your cart is empty</h1>
+          <br />
+          <StyledButton
+            text="Fill it"
+            onClickFn={() => history.push("/products")}
+          />
+        </div>
+      ) : (
+        <div>Checkout Form</div>
+      )}
       <Footer absolute="true" />
     </div>
   );
