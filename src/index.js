b8ef7fa5ccb6b89aa1d293472ab737bf7dffc262
Sreejan <sreejan147@gmail.com>
2021-09-22 20:51:19 +0530

    fix: fixed an issue

--- src/index.js
+++ src/index.js
@@ -29,6 +29,7 @@ const reducers = combineReducers({
 const persistConfig = {
   key: "root",
   storage,
+  blacklist: ["products"]
 };
 
 const persistedReducer = persistReducer(persistConfig, reducers);
