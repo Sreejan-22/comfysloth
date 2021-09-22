b79fa7e685de36ccda5fb7538cf9a58c5e7a0de3
Sreejan <sreejan147@gmail.com>
2021-09-22 22:03:32 +0530

    refactor: refactor & rearrangement

--- src/Auth0Provider.js
+++ src/Auth0Provider.js
@@ -1,26 +1,15 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
-require("dotenv").config();
 
 const Auth0ProviderWithHistory = ({ children }) => {
-  // const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-  const domain = "dev-dxbfnnd1.us.auth0.com";
-  // const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-  const clientId = "Ku0uJtvTti1Lv0cPvKBEzIhvFoCoCex0";
-
-  const history = useHistory();
-
-  // const onRedirectCallback = (appState) => {
-  //   history.push(appState?.returnTo || window.location.pathname);
-  // };
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
   return (
     <Auth0Provider
       domain={domain}
       clientId={clientId}
       redirectUri={window.location.origin}
-      // onRedirectCallback={onRedirectCallback}
     >
       {children}
     </Auth0Provider>
